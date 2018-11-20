c6ccc9b8a06481c75b3a56a2e32adf43f50ae098
Ronan Corr <ronan_corr@hotmail.com>
2018-11-20 16:43:43 -0500

    remove captcha from form as characters are not showing up

--- add-contest-form.php
+++ add-contest-form.php
@@ -52,11 +52,11 @@ function psc_photo_submit_uploader_callback( $atts ) {
 		Your Photo: <input type="file" name="image" value="<?php echo $image; ?> size="25" /><br/>
 		<!-- Post variable to indicate user-submission items -->
 		<input type="hidden" name="psc_photo_contest_submission" value="1" />
-		Re-type the following text<br/>
+		<!--Re-type the following text<br/>
                         
                         <img src="' . plugins_url(
                                 'EasyCaptcha/easycaptcha.php', __FILE__ ) . '" /> <br/>
-		<input type="text" name="contest_submission_captcha" />
+		<input type="text" name="contest_submission_captcha" />-->
 		
 		<input type="submit" name="submit" value="Submit" class="button-primary" />
 
